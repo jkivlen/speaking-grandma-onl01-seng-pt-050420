e3bfd3344358c79e817ff7d5e872b2c077ad64d5
ambitious-module-5078 <jimmykivlen@gmail.com>
2020-05-09 16:50:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
-    return 
-  elseif phrase != phrase.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
